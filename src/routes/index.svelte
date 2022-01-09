390e375981f460d7c17378a8efac89d6ea0ee037
TPolito <0613.tyler@gmail.com>
2022-01-08 23:45:37 -0600

    update about import to fix build errror

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import About from '../components/home/About.svelte';
 	import Activity from '../components/home/Activity.svelte';
 	import Contact from '../components/home/Contact.svelte';
 	import Hero from '../components/home/Hero.svelte';
 	import Footer from '../components/home/Footer.svelte';
+	import About from '../components/home/About.svelte';
 </script>
 
 <Hero />
