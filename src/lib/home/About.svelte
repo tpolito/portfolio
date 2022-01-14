84aaff615289daeea7ce44402a5b0d42ac6bd361
TPolito <0613.tyler@gmail.com>
2022-01-14 09:03:47 -0600

    fix typo in about section

--- src/lib/home/About.svelte
+++ src/lib/home/About.svelte
@@ -9,7 +9,7 @@
 				<p>
 					Hello! 👋 My name is Tyler, a software engineer from Texas. I love creating small,
 					(hopefully) useful side projects so they can exist somewhere other than in my head. I have
-					a passion for learning about technology old and new. I usually by creating small apps.
+					a passion for learning about technology old and new. Usually by creating small apps.
 				</p>
 				<p>
 					Outside the realm of technology I am an avid rockclimber, and love the outdoors. I have a
