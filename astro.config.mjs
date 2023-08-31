a8d6dd14b0aab8d87960705bf7b114e42d78e23b
Tyler Polito <things@tpolitolcom>
2023-08-31 09:23:07 -0500

    update to astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import mdx from '@astrojs/mdx';
 import pandacss from '@pandacss/dev/astro';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), svelte(), pandacss()]
+	integrations: [svelte(), pandacss()]
 });
--- src/content/blog/kings-of-the-wyld.md
+++ src/content/blog/kings-of-the-wyld.md
@@ -6,3 +6,5 @@ title: Kings of the Wyld is the best bad book I have ever read
 category: media
 tag:
 ---
+
+Imagine a novel about a bunch of DnD protag
