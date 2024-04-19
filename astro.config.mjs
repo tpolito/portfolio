8b1c2ccab33d8a46700fe9febf6a745cfa04eb8c
Tyler Polito <things@tpolitolcom>
2024-04-19 16:55:50 -0500

    update config + update 2023 review

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import pandacss from '@pandacss/dev/astro';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [svelte(), pandacss(), mdx()]
+	integrations: [svelte(), pandacss(), mdx()],
+	image: {
+		service: passthroughImageService()
+	}
 });
